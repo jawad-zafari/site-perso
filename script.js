c085d3066350425a7f557eb8c27b0b5cec4616d6
jawad zafari <jawad.zafari@laplateforme.io>
2026-04-21 12:26:57 +0200

    Gérer le menu mobile

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ window.addEventListener('load', function() {
         loader.classList.add("hidden");
     }, 400);
 });
+
+//  GÉRER LE MENU MOBILE ---
+let btnMobile = document.querySelector('.mobile-menu');
+let links = document.querySelector('.nav-links');
+
+btnMobile.onclick = function() {
+    links.classList.toggle('active');
+    btnMobile.classList.toggle('active');
+};
